cd11e68999a57b8936fcee72b6525bae9f44dc7d
Mugabi T <trevortravis91@gmail.com>
2024-10-14 20:55:15 +0300

    added run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from main import *
 
-if (OutPutFileName is None ) or (InputFilePath is None):
-    rprint("Go to the conf.py file and Enter the input file name and output file name")
-else:
-    
-    rprint(f">>[Status]: Starting to clean your file. Standby....")
-    rprint(f">>[Status]: Starting to clean your file. Standby..........")
-    main()
-    rprint(f"Thank you for using this tool...")
-    rprint(f"Reach me on Email:{my_email} for a custom made tool just for you....")
+if __name__ == "__main__":
+    if (OutPutFileName is None ) or (InputFilePath is None):
+        rprint("Go to the conf.py file and Enter the input file name and output file name")
+    else:
+        rprint(f">>[Status]: Starting to clean your file. Standby....")
+        rprint(f">>[Status]: Starting to clean your file. Standby..........")
+        main()
+        rprint(f"Thank you for using this tool...")
+        rprint(f"Reach me on Email:{my_email} for a custom made tool just for you....")
